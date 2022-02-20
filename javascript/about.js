84a2e4241aeefb4424510bdced62076bb6dec805
Pranay <pranayrapartiwar2911@gmail.com>
2022-02-20 17:35:22 +0530

    Updated Project Section

--- javascript/about.js
+++ javascript/about.js
@@ -7,8 +7,8 @@ window.addEventListener('load', function() {
         let expSection = document.querySelector('.expSection');
         for(i=0; i<data.length; i++){
             expSection.innerHTML += `<div class="expCard">
-                                        <span>${data[i].year}</span>
-                                        <span>[${data[i].company}]</span>
+                                        <span class="expYear">${data[i].year}</span>
+                                        <span class="companyName">${data[i].company}</span>
                                         <p>${data[i].experience}</p>
                                     </div>`
         }
